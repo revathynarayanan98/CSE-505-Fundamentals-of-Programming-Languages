d7a5fcff0ed60fc2eb84048159d0b8bfeabd2c81
Hemant Koti <13030557+HemantKoti@users.noreply.github.com>
2021-04-27 16:39:06 -0400

    Added A3 Problem 2

--- Assignment 3/A3_Problem2/analyzer.pl
+++ Assignment 3/A3_Problem2/analyzer.pl
@@ -1,26 +1,29 @@
 % Fill out this file for A3 Problem 2.
 
 subclass(C1, C2) :-
-      _________________.
+      db_class(C1, C2).
 
 subclass(C1, C2) :-
-      _________________,
-      _________________.
+      db_class(X, C2),
+      subclass(C1, X).
 
+% recursive(C) :-
+%       db_field(X, Y:C).
 
 recursive(C) :-
-      _________________.
-
-recursive(C) :-
-      ________________,
-      ________________.
+      subclass(X, C),
+      db_field(X, Y:C).
 
 over_ridden(B, C, M, T) :-   
-       _________________,
-       _________________,
-       _________________,
-       _________________.
+      subclass(X,C),
+      (B==X;subclass(B,X)),
+      db_method(C,M,T),
+      db_method(X,M,T).
 
+helper(C, X, M, T) :-
+      subclass(C, X),
+      db_method(X, M, T),
+      \+over_ridden(C, X, M, T).
 
 inherits(C,L) :-
       % use setof and \+ over_ridden
@@ -29,35 +32,33 @@ inherits(C,L) :-
       % 
       % Use print_list(L) shown below
       % for printing out your answer.
+      setof(X:M:T, helper(C,X,M,T), L),
+      print_list(L).
 
 print_list([]).
 print_list([H|T]) :- write(H), nl, print_list(T).
 
-
 % basic is true T is a built-in type; needed by cycle
 basic(T) :-
-      _____________________.
+      builtin(T).
+      
+cycle(C) :-
+      db_method(C,X,(K->Y)),
+      member(Z,K),
+      \+basic(Z),
+      cycle(Z,C,[Z]).
 
 cycle(C) :-
-      db_method(____, ____, ____),
-      member(_____, _____).
-cycle(C) :-
-      _________________,
-      _________________,
-      _________________,
-      cycle(___, ___, ___).
+       db_method(X,Y,(Z->K)),
+       member(C,Z).
 
-cycle(C,C0,_) :-
-       _________________,
-       _________________.
+cycle(C,C0,CX) :-
+      db_method(C,X,(Y->K)),
+      member(C0,Y).
+
 cycle(C,C0,CL) :-
-       __________________,
-       __________________,
-       __________________,
-       __________________,
-       cycle(___, ___, ___).
-
-
-
-
-
+      db_method(C,X,(Y->K)),
+      member(Z,Y),
+      \+basic(Z),
+      \+member(Z,CL),
+      cycle(Z,C0,[Z,CL]).
